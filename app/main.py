b53ef2503b1ce86e2b0bb5bed9f4f14a2e01676a
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-03-24 12:30:00 +0100

    initial dummy values and localhost cors

--- app/main.py
+++ app/main.py
@@ -1,23 +1,37 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/simulations")
 async def read_simulations():
     return [{"id": 1,
-             "participantId": 2,
-             "running": False,
-             "timeRunning": "00:00",
+             "participantId": "DIMITRI",
+             "running": True,
+             "timeRunning": "13:40",
              "timeTotal": "12:40",
              "song": "test - song"
              },
-            {"id": 1,
-             "participantId": 2,
+            {"id": 2,
+             "participantId": "ANJA",
              "running": False,
              "timeRunning": "00:00",
-             "timeTotal": "12:40",
-             "song": "test - song"
+             "timeTotal": "29:30",
+             "song": "-"
              }
             ]
 
@@ -25,7 +39,7 @@ async def read_simulations():
 @app.get("/simulations/{sim_id}")
 async def read_simulation(sim_id: int):
     return {"id": sim_id,
-            "participantId": 2,
+            "participantId": "DIMITRI",
             "running": False,
             "timeRunning": "00:00",
             "timeTotal": "12:40",
