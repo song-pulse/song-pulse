99b2f897f670a4e827ba8bdd1e876f7cc7632108
Anja Koller <anja.koller@uzh.ch>
2020-04-07 20:55:13 +0200

    New changes qlearning and datareading

---
+++ app/mltraining/music_mapping.py
@@ -0,0 +1,12 @@
+def main(self):
+    def map_state_to_action():
+        return
+
+    def map_state_action_to_reward():
+        return
+
+    def transition():
+        return
+
+    if __name__ == "__main__":
+        main()
--- app/mltraining/q_learning_music.py
+++ app/mltraining/q_learning_music.py
@@ -1,58 +1,85 @@
 import numpy as np
 
-
-def main():
-    print('hello world')  # just for testing purpose
-    # TODO: 1) read in data (HRV/EDA or both)
-    alpha = 0.5  # between 0 and 1
-    gamma = 0.5  # sth between 0 and 1
-    states = [0, 1, 2]  # 3 stress states: 0 means below baseline stress, 1 baseline stress, 2 above baseline stress
-    actions = [0, 1, 2]  # 3 different actions: lower music, stay same, upper music
-    rewards = [-1, 0, 1]  # possible rewards (the bigger the reward the better)
-    Q_table = np.zeros((len(states), len(actions)))
-    print('Q_table', Q_table)
-    set_parameters(alpha, gamma, states, actions, rewards)
-    # TODO: define userrating mapping and how to integrate (scores fom 1 to 5 --> mapped to rewards) TODO: define
-    #  timesteps at which we get the data -> either HRV/EDA need to be combined and need to be same TODO: for
-    #   simplicity just take one of the two parameters and fix timesteps at which we get the data TODO: 2) set
-    #    parameters (alpha, gamma, ...) TODO: 3) define stateset, actionset, transitionfunction etc. TODO: 3) define
-    #     update steps for actions, policy etc (as we have to build a new env, not like in gym) TODO: 4) run q
-    #      learning for normal data TODO 5) run q learning for feedback TODO: 6) define episode and terminal state
-    #       for alg -> an episode is defined through a cerain timestep TODO the terminal state is when the
-    #        stresslevel stays at the perfect level or when all data is finished (we have gone through)
+# alpha, gamma and epsilon are values between 0 and 1
+EPSILON = 0.5
 
 
-def read_in_data():
-    return
+class SongPulseAgent:
+    def __init__(self, alpha=0.5, gamma=0.5, num_episodes=10):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.num_episodes = num_episodes
+        self.states = [0, 1, 2]  # 0 means below baseline stress, 1 baseline stress, 2 above baseline stress
+        self.actions = [0, 1, 2]  # 3 different actions: lower music, stay same, upper music
+        self.rewards = [-1, 0, 1]  # possible rewards (the bigger the reward the better)
+        self.reward = self.rewards[1]  # default reward is 0
+        self.state = self.states[1]  # default state is baseline stress (state 1)
+        self.action = self.actions[1]  # default action is doing nothing (action 1)
+        print('stateslen', (len(self.states)))
+        self.n_states = len(self.states)
+        self.n_actions = len(self.actions)
+        self.Q_table = np.zeros((self.n_states, self.n_actions), dtype=float, order='C')
+        print('Q_table', self.Q_table)
 
+    def update_q_table(self, state, action, reward, new_state):
+        # update Q table: Q(s,a) = Q(s,a) + alpha[R + gamma maxa Q(S',a) -Q(S,A)]
+        # q table is a table with all possible states s on the y axis and the possible actions a on the x axis
+        # the entries at (a,s) denote the expected reward you get when being in state s and take action a
+        self.Q_table[state, action] += self.alpha * (
+                reward + self.gamma * np.max(self.Q_table[new_state]) - self.Q_table[state][action])
+        return
 
-def set_parameters(alpha, gamma, states, actions, rewards):
-    alpha = alpha
-    gamma = gamma
-    a0 = actions[1]  # default action is doing nothing (action 1)
-    s0 = states[1]  # default state is baseline stress (state 1)
-    r0 = rewards[1]  # default reward is 0
+    def next_state_func(self):
+        # transition function: input is a given state and action and this function returns the next_state when taking
+        # action transition table -> position state,action entry denotes next state
+        Transition_matrix = np.zeros((self.n_states, self.n_actions), dtype=float, order='C')
+        Transition_matrix[0, 0] = 0
+        Transition_matrix[0, 1] = 0
+        Transition_matrix[0, 2] = 1
+        Transition_matrix[1, 0] = 0
+        Transition_matrix[1, 1] = 1
+        Transition_matrix[1, 2] = 2
+        Transition_matrix[2, 0] = 1
+        Transition_matrix[2, 1] = 2
+        Transition_matrix[2, 2] = 2
+        print('transition matrix', Transition_matrix)
+        new_state = Transition_matrix[self.state, self.action]
+        return new_state
 
+    def get_new_reward(self):
+        # shows return for taking step from (s,a) to (s',a)
+        # make a reward -state-action matrix for this
+        # TODO: self.reward =
+        return self.reward
 
-def update_q_table():
-    # TODO: see algorithm q learning
-    # q table is a table with all possible states s on the y axis and the possible actions a on the x axis
-    # the entries at (a,s) denote the expected reward you get when being in state s and take action a
-    return
+    def choose_action(self, epsilon=EPSILON):
+        if np.random.random() < epsilon:
+            # randomly sample explore_rate percent of the time
+            return np.random.choice(self.actions)
+        else:
+            # take optimal action
+            return np.argmax(self.Q_table[self.state])
 
+    def train(self):
+        # simulate execution with rewards that we would get and train the model before running
+        for e in range(self.num_episodes):
+            done = False
 
-def next_state(state):
-    # TODO: input is a given stte and this function returns the nextstate (transitionfunction)
-    return
+            while not done:
+                action = self.choose_action(self.state)
+                new_state = self.next_state_func()
+                reward = self.get_new_reward()
+                # TODO: define reward, retrieve done variable, and new_state
+                self.update_q_table(self.state, action, reward, new_state=new_state)
+                self.state = new_state
+            print('training finished')
 
-
-def choose_action():
-    return
-
-
-def run__q_learning():
-    return
+    def run(self):
+        # TODO:
+        return
 
 
 if __name__ == "__main__":
-    main()
+    agent = SongPulseAgent()
+    agent.train()
+    agent.run()
