0be3909ab633752344d4f0846fa2adef7aa96d5c
Anja Koller <anja.koller@uzh.ch>
2020-04-17 16:48:17 +0200

    Fix qlearning

--- app/mltraining/q_learning_music.py
+++ app/mltraining/q_learning_music.py
@@ -8,31 +8,29 @@ NUM_TRAINING = 10
 
 
 class SongPulseAgent:
-    def __init__(self, alpha=ALPHA, gamma=GAMMA, num_training=NUM_TRAINING, epsilon= EPSILON):
+    def __init__(self, alpha=ALPHA, gamma=GAMMA, num_training=NUM_TRAINING, epsilon=EPSILON):
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
         self.num_training = num_training
         self.states = [0, 1, 2]  # 0 means below baseline stress, 1 baseline stress, 2 above baseline stress
         self.actions = [0, 1, 2]  # 3 different actions: lower music, stay same, upper music
-        self.rewards = [-1, 0, 1]  # possible rewards (the bigger the reward the better)
+        self.rewards = [0, 1]  # possible rewards (the bigger the reward the better)
         self.reward = self.rewards[1]  # default reward is 0
-        self.state = self.states[1]  # default state is baseline stress (state 1)
+        self.state = self.states[2]  # default state is baseline stress (state 1)
         self.new_state = self.state  # initially new_state = state
         self.action = self.actions[1]  # default action is doing nothing (action 1)
-        print('states len', (len(self.states)))
         self.n_states = len(self.states)
         self.n_actions = len(self.actions)
         self.Q_table = np.zeros((self.n_states, self.n_actions), dtype=int, order='C')
-        print('Q_table', self.Q_table)
 
     def get_adaptive_epsilon(self, e):
         # return max(min_epsilon, min(1, 1.0 - np.math.log10((episode + 1) / number_of_states)))
         if e < (self.num_training / 10):
-            return 1.0
+            self.epsilon = 1.0
         if e > self.num_training * 0.9:
-            return 0.0
-        return EPSILON
+            self.epsilon = 0.0
+        return self.epsilon
 
     def update_q_table(self, state, action, reward, new_state):
         # update Q table: Q(s,a) = Q(s,a) + alpha[R + gamma maxa Q(S',a) -Q(S,A)]
@@ -55,10 +53,9 @@ class SongPulseAgent:
         Transition_matrix[2, 0] = 1
         Transition_matrix[2, 1] = 2
         Transition_matrix[2, 2] = 2
-        print('transition matrix', Transition_matrix)
-        new_state = Transition_matrix[self.state, self.action]
-        print('new state', new_state)
-        return new_state
+        self.new_state = Transition_matrix[self.state, self.action]
+        print('new state', self.new_state)
+        return self.new_state
 
     def choose_action(self, epsilon=EPSILON):
         if np.random.random() < epsilon:
@@ -67,6 +64,16 @@ class SongPulseAgent:
         else:
             # take optimal action
             return np.argmax(self.Q_table[self.state])
+
+    def get_reward(self):
+        # TODO: discuss how rewarding scheme should be done
+        # if new_state is better than state positive reward, else neg reward -> state s is best
+        # positive if from s0 to s1 or from s2 to s1
+        if(self.new_state == 1):
+            self.reward = 1
+        else:
+            self.reward = 0
+        return self.reward
 
     def train(self):
         # simulate execution with rewards that we would get and train the model before running
@@ -77,13 +84,12 @@ class SongPulseAgent:
                 print('old state', self.state)
                 self.epsilon = self.get_adaptive_epsilon(e)
                 self.action = self.choose_action(self.state)
-                self.new_state = self.next_state_func()  # TODO: look at this as we get the next state also from
-                # measure, this is only what we expect, so maybe get next_state should go to the server -> not
-                # possible due to real time issue? not predictable -> learn with already there data
-                reward = self.Q_table[self.state, self.action]
-                print('reward', reward)
-                # TODO: define reward, retrieve done variable, and new_state
-                self.update_q_table(self.state, self.action, reward, new_state=self.new_state)
+                # TODO: here next state should be computed differently
+                self.new_state = self.next_state_func()
+                self.reward = self.get_reward()
+                print('reward is', self.reward)
+                self.update_q_table(self.state, self.action, self.reward, new_state=self.new_state)
+                print('qtable after update', self.Q_table)
                 self.state = self.new_state
                 print('new state', self.state)
             print('training finished')
@@ -113,3 +119,8 @@ if __name__ == "__main__":
     agent.run(num_adaptions)
 # TODO: difference to other q learning algs -> the state should not get updated in the run phase as the state comes from
 # our measurements not and cannot be fully determined by us
+
+# TODO: define training and run data -> with the training data we now and get the next state and reward -> with the
+# run data we don't but we just act according to the created policy
+
+# TODO: later compare with basic strategy music mapping
