b0bd562b79a304318d9bc84f5b48bf12f2aa9a33
kathriwa <kathrin.wardatzky@uzh.ch>
2020-05-14 12:28:56 +0200

    Get settings from database and add logic

--- app/preprocessing/learning_wrapper.py
+++ app/preprocessing/learning_wrapper.py
@@ -10,16 +10,7 @@ class LearningWrapper:
         self.tempCount = 0
         self.tempBaseline = 0
         self.learning = SongPulseAgent()
-        self.cleaning = DataCleaning(smoothing=3,
-                                     eda_baseline='mean',
-                                     eda_threshold=0.01,
-                                     acc_threshold=0.5,
-                                     acc_latency=3,
-                                     temp_baseline=0.3,
-                                     temp_latency=3,
-                                     min_duration=3,
-                                     upper_stress_threshold=0.1,
-                                     lower_stress_threshold=-0.1)  # TODO remove / get them from the database
+        self.cleaning = DataCleaning()
 
     def calculate_eda_baseline(self, current_value):  # TODO get baseline from database
         if self.edaBaseline == 0:
@@ -40,4 +31,5 @@ class LearningWrapper:
         self.calculate_temp_baseline(data.edaValue)
 
         tendency = self.cleaning.run(data, self.edaBaseline, self.tempBaseline)
+        print(tendency)
         return self.learning.run_with_tendency(tendency)
