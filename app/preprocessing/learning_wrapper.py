dc8446bf45d9e2f598567f5c09e3c51cb25e0b04
kathriwa <kathrin.wardatzky@uzh.ch>
2020-05-27 22:10:02 +0200

    Add ibi baseline

--- app/preprocessing/learning_wrapper.py
+++ app/preprocessing/learning_wrapper.py
@@ -9,6 +9,8 @@ class LearningWrapper:
         self.edaBaseline = 0
         self.tempCount = 0
         self.tempBaseline = 0
+        self.ibiCount = 0
+        self.ibiBaseline = 0
         self.learning = SongPulseAgent()
         self.cleaning = DataCleaning()
 
@@ -17,19 +19,28 @@ class LearningWrapper:
             self.edaBaseline = current_value
         else:
             self.edaBaseline = ((self.edaBaseline * self.edaCount) + current_value) / (self.edaCount + 1)
-        self.edaCount = self.edaCount + 1
+        self.edaCount += 1
 
     def calculate_temp_baseline(self, current_value):
+        # TODO not needed at the moment, include when needed
         if self.tempBaseline == 0:
             self.tempBaseline = current_value
         else:
-            self.tempBaseline = ((self.tempBaseline * self.edaCount) + current_value) / (self.edaCount + 1)
-        self.edaCount = self.edaCount + 1
+            self.tempBaseline = ((self.tempBaseline * self.tempCount) + current_value) / (self.tempCount + 1)
+        self.tempCount += 1
 
-    def run(self, data):
+    def calculate_ibi_baseline(self, current_value):
+        if self.ibiBaseline == 0:
+            self.ibiBaseline = current_value
+        else:
+            self.ibiBaseline = ((self.ibiBaseline * self.ibiCount) + current_value) / (self.ibiCount + 1)
+        self.ibiCount += 1
+
+    def run(self, data, timestamp, run_id):
         self.calculate_eda_baseline(data.edaValue)
         self.calculate_temp_baseline(data.edaValue)
+        self.calculate_ibi_baseline(data.ibiValue)
 
-        tendency = self.cleaning.run(data, self.edaBaseline, self.tempBaseline)
+        tendency = self.cleaning.run(data, self.edaBaseline, self.ibiBaseline)
         print(tendency)
-        return self.learning.run_with_tendency(tendency)
+        return self.learning.run_with_tendency(tendency, timestamp, run_id)
