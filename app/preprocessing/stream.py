1e10cbbe11e813298eac8c2446e5ae5630fdba88
Anja Koller <anja.koller@uzh.ch>
2020-06-02 18:43:47 +0200

    Add changes from Thursday (merging preprocessing ml fix

--- app/preprocessing/stream.py
+++ app/preprocessing/stream.py
@@ -30,7 +30,7 @@ class Stream:
             values = crud.value.get_all_for_file(db_session, file_id=file.id)
 
             # Sort the values in the file so that they are sorted according to their timestamp.
-            values.list.sort(key=values.timestamp)
+            values.sort(key=lambda x: x.timestamp)
 
             # Store the file in the correct variable according to their sensor ID.
             if file.sensor.name.lower() == "TEMP".lower():
@@ -50,7 +50,7 @@ class Stream:
                                                                       bvp_data, eda_data,
                                                                       ibi_data, run,
                                                                       temp_data)
-            song_id = learning.run(data_for_time_object, value.timestamp, run.id)
+            song_id = learning.run(data_for_time_object, value.timestamp, run.id, db_session)
             result = ResultCreate(timestamp=value.timestamp, song_id=song_id, verdict=-1,
                                   input=str(data_for_time_object))
             crud.result.create_with_run(db_session=db_session, obj_in=result, run_id=run.id)
@@ -60,7 +60,7 @@ class Stream:
     # This methods creates a data object for a given time stamp.
     # This object can then be given to the data cleaning for processing the data.
     @staticmethod
-    async def create_data_for_time_object(value, acc_data, acc_interval1, acc_interval2, bvp_data, eda_data, ibi_data,
+    def create_data_for_time_object(value, acc_data, acc_interval1, acc_interval2, bvp_data, eda_data, ibi_data,
                                           run,
                                           temp_data):
         data_for_time = DataForTime()
@@ -70,31 +70,31 @@ class Stream:
         data_for_time.timestamp = value.timestamp
 
         # Add the values for the specific time.
-        data_for_time.edaValue = value.value
+        data_for_time.edaValue = value.value1
 
         # Iterate over the bvp values and add the value at the given time stamp.
         for bvpValue in bvp_data:
             if bvpValue.timestamp == value.timestamp:
-                data_for_time.bvpValue = bvpValue.value
+                data_for_time.bvpValue = bvpValue.value1
 
         # Iterate over the ibi values and add the value at the given time stamp.
         for ibiValue in ibi_data:
             if ibiValue.timestamp == value.timestamp:
-                data_for_time.bvpValue = ibiValue.value
+                data_for_time.bvpValue = ibiValue.value1
 
         # Iterate over the acc values and add the value at the given time stamp plus of two intervals from before.
         for accValue in acc_data:
             if accValue.timestamp == value.timestamp:
                 data_for_time.accValues.append(acc_interval1)
                 data_for_time.accValues.append(acc_interval2)
-                data_for_time.accValues.append(accValue.value)
+                data_for_time.accValues.append(accValue.value1)
             else:
                 acc_interval1 = acc_interval2
-                acc_interval2 = accValue.value
+                acc_interval2 = accValue.value1 # TODO: integrate value1,value2,value3
 
         # Iterate over the temp values and add the value at the given time stamp.
         for tempValue in temp_data:
             if tempValue.timestamp == value.timestamp:
-                data_for_time.tempValue = tempValue.value
+                data_for_time.tempValue = tempValue.value1
 
         return data_for_time
