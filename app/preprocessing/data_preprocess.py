cc74dacc90d76b4a70e05b6b980c8c010338581e
kathriwa <kathrin.wardatzky@uzh.ch>
2020-06-02 14:57:46 +0200

    Add PRR20 calculation

--- app/preprocessing/data_preprocess.py
+++ app/preprocessing/data_preprocess.py
@@ -3,26 +3,30 @@ from app import crud
 
 
 class DataCleaning(object):
-    # TODO include HRV computation and majority voting
 
     def __init__(self):
 
         self.settings = crud.setting.get(db, id=1)
         self.prev_eda_tend = deque([], maxlen=self.settings.duration)
         self.temp_data = deque([], maxlen=self.settings.temp_latency)
-        self.prev_mean_rr = deque([], maxlen=self.settings.duration)
         self.prev_eda_stress = deque([], maxlen=self.settings.duration)
+        self.prev_ibi = deque([], maxlen=self.settings.duration)
+        self.prev_mean_rr = deque([], maxlen=self.settings.duration)
         self.prev_mean_rr_stress = deque([], maxlen=self.settings.duration)
+        self.prev_mean_prr20 = deque([], maxlen=self.settings.duration)
         self.prev_prr20_stress = deque([], maxlen=self.settings.duration)
 
     def compute_mean_rr(self, ibi_value, ibi_baseline):
         relative_ibi = ibi_value - ibi_baseline
-        self.prev_mean_rr.append(relative_ibi)
-        return relative_ibi
+        self.prev_ibi.append(relative_ibi)
+        mean_rr = sum(self.prev_mean_rr)/float(len(self.prev_mean_rr))
+        self.prev_mean_rr.append(mean_rr)
+        return mean_rr
 
     def compute_prr20(self):
-        # TODO
-        pass
+        prr20 = ((len([i for i in self.prev_ibi if i >= 20])) * 100) / float(len(self.prev_ibi) - 1)
+        self.prev_mean_prr20.append(prr20)
+        return prr20
 
     def detect_movement(self, acc_values):
         if acc_values[2] > acc_values[1] > acc_values[0]:
@@ -40,22 +44,28 @@ class DataCleaning(object):
         else:
             return 1
 
-    def detect_eda_change(self):
-        diff = self.prev_eda_tend[-1] - self.prev_eda_tend[0]
-        # detect and increase if values have been increasing constantly and the difference is above threshold
-        if all(i < j for i, j in zip(self.prev_eda_tend, self.prev_eda_tend[1:])) and diff >= self.settings.eda_threshold:
-            return True
-        elif all(i > j for i, j in zip(self.prev_eda_tend, self.prev_eda_tend[1:])) and diff >= self.settings.eda_threshold:
-            return True
-        else:
-            return False
+    def validate_stress_level(self, data, prev_values, prev_stress, change, stress_threshold):
+        stress_level = 1
+        if len(prev_values) >= self.settings.duration:
+            if not self.detect_movement(data.acc_values):
+                if change:
+                    stress_level = self.detect_stress_level(prev_values[-1], stress_threshold)
+                else:
+                    stress_level = prev_stress[-1]
+            else:
+                if not self.detect_movement(data.acc_values):
+                    if prev_values[-1] > stress_threshold:
+                        stress_level = 2
+                    elif prev_values[-1] < (stress_threshold * -1):
+                        stress_level = 0
+        return stress_level
 
-    def detect_mean_rr_change(self):
-        # TODO add threshold
-        # diff = self.prev_mean_rr[-1] - self.prev_mean_rr[0]
-        if all(i < j for i, j in zip(self.prev_mean_rr, self.prev_mean_rr[1:])):
+    def detect_change(self, prev_values, threshold):
+        diff = prev_values[-1] - prev_values[0]
+        # detect and increase if values have been increasing constantly and the difference is above threshold
+        if all(i < j for i, j in zip(prev_values, prev_values[1:])) and diff >= threshold:
             return True
-        elif all(i > j for i, j in zip(self.prev_mean_rr, self.prev_mean_rr[1:])):
+        elif all(i > j for i, j in zip(prev_values, prev_values[1:])) and diff >= threshold:
             return True
         else:
             return False
@@ -67,48 +77,30 @@ class DataCleaning(object):
         return majority
 
     def run(self, data, eda_baseline, ibi_baseline):
-        # return 1 value per loop
+        # EDA
         eda_tend = data.edaValue - eda_baseline
         self.prev_eda_tend.append(eda_tend)
 
-        # for the first values where it is not possible to compare with the previous eda values and check the previous
-        # acc, it takes the relative values only
-        eda_stress = 1
-        if len(self.prev_eda_tend) >= self.settings.duration:
-            if not self.detect_movement(data.acc_values):
-                if self.detect_eda_change():
-                    eda_stress = self.detect_stress_level(eda_tend, self.settings.stress_threshold)
-                else:
-                    eda_stress = self.prev_eda_stress[-1]
-        else:
-            if not self.detect_movement(data.acc_values):
-                if eda_tend > self.settings.stress_threshold:
-                    eda_stress = 2
-                elif eda_tend < (self.settings.stress_threshold * -1):
-                    eda_stress = 0
+        eda_change = self.detect_change(self.prev_eda_tend, self.settings.eda_threshold)
+        eda_stress = self.validate_stress_level(data, self.prev_eda_tend, self.prev_eda_stress,
+                                                eda_change, self.settings.stress_threshold)
         self.prev_eda_stress.append(eda_stress)
 
         # MeanRR
-        mean_rr = self.compute_mean_rr(data.ibi_value, ibi_baseline)
-        mean_rr_stress = 1
-        if len(self.prev_mean_rr) >= self.settings.duration:
-            if not self.detect_movement(data.acc_values):
-                if self.detect_mean_rr_change():
-                    # TODO change with mean rr threshold?
-                    mean_rr_stress = self.detect_stress_level(mean_rr, self.settings.stress_threshold)
-                else:
-                    mean_rr_stress = self.prev_mean_rr_stress[-1]
-        else:
-            if not self.detect_movement(data.acc_values):
-                # TODO exchange threshold?
-                if mean_rr > self.settings.stress_threshold:
-                    mean_rr_stress = 2
-                elif mean_rr < (self.settings.stress_threshold * -1):
-                    mean_rr_stress = 0
+        # TODO create meanRR stress threshold
+        self.compute_mean_rr(data.ibi_value, ibi_baseline)
+        mean_rr_change = self.detect_change(self.prev_mean_rr, self.settings.ibi_threshold)
+        mean_rr_stress = self.validate_stress_level(data, self.prev_mean_rr, self.prev_mean_rr_stress,
+                                                    mean_rr_change, self.settings.stress_threshold)
+
         self.prev_mean_rr_stress.append(mean_rr_stress)
 
-        # TODO compute prr20Stress
-        prr20_stress = 1
+        # PRR20
+        self.compute_prr20()
+        # TODO write function to determine change
+        # TODO include change threshold?
+        prr20_change = self.detect_change(self.prev_mean_prr20, threshold=0.0)
+        prr20_stress = self.validate_stress_level(data, self.prev_mean_prr20, self.prev_prr20_stress,
+                                                  prr20_change, self.settings.prr_threshold)
 
         return self.majority_vote(mean_rr_stress, eda_stress, prr20_stress)
-
