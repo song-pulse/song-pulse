25d32b6b755e7f8855db334bb19ceac43d96cac7
kathriwa <kathrin.wardatzky@uzh.ch>
2020-07-16 11:30:50 +0200

    Add eda threshold validation

--- app/tests/preprocessing/dummy_data.py
+++ app/tests/preprocessing/dummy_data.py
@@ -3,7 +3,7 @@ class Settings:
         self.duration = 3
         self.temp_latency = 2
         self.acc_threshold = 18
-        self.eda_threshold = 0.3
+        self.eda_threshold = 2.3
         self.ibi_threshold = 0.4
         self.prr_threshold = 0.5
         self.stress_threshold = 0.5
