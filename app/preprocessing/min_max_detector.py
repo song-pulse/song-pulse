9044f3f2e7a107fbe10a3bfd83a254f627997caf
Dimitri Kohler <dimitri.kohler@bf.uzh.ch>
2020-07-20 14:31:36 +0200

    #28 improved prr20

--- app/preprocessing/min_max_detector.py
+++ app/preprocessing/min_max_detector.py
@@ -10,12 +10,14 @@ def createBaselineUpdate(baseline: Baseline, new_min_max: float, min_changed: bo
         new_min_max = (new_min_max + baseline.min_value) / 2
         return BaselineUpdate(participant_id=baseline.participant_id,
                               sensor_id=baseline.sensor_id,
+                              counter=baseline.counter + 1,
                               min_value=new_min_max
                               )
     if max_changed:
         new_min_max = (new_min_max + baseline.max_value) / 2
         return BaselineUpdate(participant_id=baseline.participant_id,
                               sensor_id=baseline.sensor_id,
+                              counter=baseline.counter + 1,
                               max_value=new_min_max
                               )
 
@@ -30,14 +32,14 @@ def checkMinMaxChange(min_value: float, max_value: float, current_value: float):
 
 def checkMinMax(min_value: float, max_value: float, current_value: float):  # TODO is 25% good? should it be 10%?
     total_range = max_value - min_value
-    current_position = current_value - min_value
+    percentile10 = total_range / 10  # 10% from bottom
+    percentile2 = total_range / 50  # 2% from top
+    top = max_value - percentile2
+    bottom = min_value + percentile10
 
-    bottom_quarter = total_range / 4  # bottom 25%
-    top_quarter = bottom_quarter * 3  # bottom 75%
-
-    if current_position <= bottom_quarter:
+    if current_value <= bottom:
         return 0
-    if current_position >= top_quarter:
+    if current_value >= top:
         return 2
     else:
         return 1
@@ -48,6 +50,15 @@ def createNewBaselines(db_session: Session, part_id: int):
     for sensor in sensors:
         crud.baseline.create(db_session=db_session,
                              obj_in=BaselineCreate(sensor_id=sensor.id, participant_id=part_id))
+
+
+def update_baseline_if_needed(baseline, db_session, prr_20_value):
+    if baseline.counter < 30:  # after about 5 minutes of calibration we're fixed
+        max_changed, min_changed = checkMinMaxChange(baseline.min_value, baseline.max_value, prr_20_value)
+        if max_changed or min_changed:
+            crud.baseline.update(db_session=db_session,
+                                 db_obj=baseline,
+                                 obj_in=createBaselineUpdate(baseline, prr_20_value, max_changed, min_changed))
 
 
 class MinMaxDetector:
@@ -66,24 +77,12 @@ class MinMaxDetector:
         for baseline in baselines:
             if baseline.sensor.name == "EDA":  # EDA
                 eda_tendency = checkMinMax(baseline.min_value, baseline.max_value, eda_value)
-                max_changed, min_changed = checkMinMaxChange(baseline.min_value, baseline.max_value, eda_value)
-                if max_changed or min_changed:
-                    crud.baseline.update(db_session=db_session,
-                                         db_obj=baseline,
-                                         obj_in=createBaselineUpdate(baseline, eda_value, max_changed, min_changed))
+                update_baseline_if_needed(baseline, db_session, eda_value)
             elif baseline.sensor.name == "IBI":  # MEAN RR
                 mean_rr_tendency = checkMinMax(baseline.min_value, baseline.max_value, mean_rr_value)
-                max_changed, min_changed = checkMinMaxChange(baseline.min_value, baseline.max_value, mean_rr_value)
-                if max_changed or min_changed:
-                    crud.baseline.update(db_session=db_session,
-                                         db_obj=baseline,
-                                         obj_in=createBaselineUpdate(baseline, mean_rr_value, max_changed, min_changed))
+                update_baseline_if_needed(baseline, db_session, mean_rr_value)
             elif baseline.sensor.name == "ACC":  # PRR 20 # TODO don't abuse ACC for prr 20 values, perhaps decoulpe baselines from sensors?
                 prr_20_tendency = checkMinMax(baseline.min_value, baseline.max_value, prr_20_value)
-                max_changed, min_changed = checkMinMaxChange(baseline.min_value, baseline.max_value, prr_20_value)
-                if max_changed or min_changed:
-                    crud.baseline.update(db_session=db_session,
-                                         db_obj=baseline,
-                                         obj_in=createBaselineUpdate(baseline, prr_20_value, max_changed, min_changed))
+                update_baseline_if_needed(baseline, db_session, prr_20_value)
 
         return eda_tendency, mean_rr_tendency, prr_20_tendency
